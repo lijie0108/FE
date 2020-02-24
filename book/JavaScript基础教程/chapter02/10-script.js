b8652df38c71441d120e236c79c99d2e15d36d0c
v_ljinjieli <2753482010@qq.com>
2020-02-24 08:58:31 +0800

    更新

---
+++ book/JavaScript基础教程/chapter02/10-script.js
@@ -0,0 +1,13 @@
+window.onload = initAll;
+function initAll() {
+  var ans = prompt("Enter a number", "");
+  try {
+    if(!ans || isNaN(ans) || ans < 0) {
+      throw new Error("Not a valid number");
+    }
+    alert("The square root of " + ans + " is " + Math.sqrt(ans));
+  }catch(errMsg){
+    console.log(errMsg);
+    alert(errMsg.message);
+  }
+}
